83a24a421b2dee6063e81e08c2f8c1cbbef434d5
RyaanHassanKhan <106651075+RyaanHassanKhan@users.noreply.github.com>
2022-07-29 15:51:43 +0530

    Add files via upload

---
+++ enter element in array.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+    { int x[4],i;
+    printf("enter the values for array");
+    for (i=0; i<4; i++)
+    scanf("%d", & x[i]);
+    
+    for (i=0; i<4; i++)
+     printf("%d", x[i]);
+    return 0;
+	}
