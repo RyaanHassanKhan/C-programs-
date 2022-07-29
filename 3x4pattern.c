4bf4a842611b66dcb0f397689a2b2f70d809f162
RyaanHassanKhan <106651075+RyaanHassanKhan@users.noreply.github.com>
2022-07-29 16:03:17 +0530

    Add files via upload

---
+++ 3x4pattern.c
@@ -0,0 +1,15 @@
+/* Program in C to print 3x4 pattern */
+#include <stdio.h>
+int main()
+{
+	int i,j;
+	for (i=1;i<=3;i++)
+	{
+		for (j=1;j<=4;j++)
+		{
+			printf("*");
+		}
+	printf("\n");
+	}
+	return 0;
+}
